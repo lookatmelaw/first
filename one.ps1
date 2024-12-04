cab8ffba66504e81dd605ff5528ef1ac92206c91
lookatmelaw <murtesa991@gmail.com>
2024-12-04 21:09:37 +0300

    Update one.ps1

--- one.ps1
+++ one.ps1
@@ -1,10 +1,5 @@
-# Set the Discord webhook URL
 $webhookUrl = 'https://discord.com/api/webhooks/1312716594543329310/AESsDL5qvv4f0SIzKBjnUbZtDC-nyv5k4yuMfZIJsbtOLAJGo08MIV-j_PMD1hhjT5SL'
-
-# Set the payload with the message you want to send
 $payload = @{
     content = 'Hello'
 } | ConvertTo-Json
-
-# Send the POST request to the webhook URL
 Invoke-RestMethod -Uri $webhookUrl -Method Post -ContentType 'application/json' -Body $payload
